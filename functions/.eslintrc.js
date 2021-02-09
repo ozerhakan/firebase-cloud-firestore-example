56dd2f65a5bf0f0f1be092deebd5ebe1f01b1eae
example <hakanzr@gmail.com>
2021-02-09 20:33:14 +0300

    .init

---
+++ functions/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'google',
+  ],
+  rules: {
+    'promise/no-callback-in-promise': 'off',
+    'promise/param-names': 'off',
+    'promise/catch-or-return': 'off',
+    'class-methods-use-this': 'off',
+    'indent': ['error', 2, {
+      'ignoredNodes': ['TemplateLiteral'],
+    }],
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', 140, 2],
+    'no-bitwise': 'off',
+    'linebreak-style': 'off',
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+      },
+    ],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+    'func-names': 'off',
+    'object-curly-spacing': 'off',
+  },
+};
---
+++ functions/index.js
@@ -0,0 +1,68 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const firebase = require('firebase-admin');
+firebase.initializeApp();
+
+
+const app = express();
+app.use(cors({ origin: true }));
+
+const productRef = firebase.firestore().collection('products');
+
+app.post('/product', async (req, res) => {
+    const product = req.body;
+
+    await productRef.add(product)
+        .then((result) => {
+            res.status(200).send(`Product added: ${result.id}`);
+        }).catch((error) => {
+            res.status(500).send(error);
+        })
+});
+
+app.get('/products', async (req, res) => {
+    await productRef.get()
+        .then((snapshot => {
+            const products = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            res.status(200).send(products);
+    })).catch((error) => {
+            res.status(500).send(error);
+        })
+});
+
+app.get("/product/:id", async (req, res) => {
+
+    await productRef.doc(req.params.id).get()
+        .then(product => {
+            if(!product.exists) {
+                throw new Error('Product not found');
+            }
+            res.status(200).json(product.data())
+        }).catch(error =>
+            res.status(500).send(error));
+
+});
+
+app.put("/product/:id", async (req, res) => {
+
+    await productRef.doc(req.params.id).update(req.body)
+        .then(() => {
+            res.status(200).send("Product successfully updated !")
+        }).catch(error => res.status(500).send(error));
+});
+
+app.delete("/product/:id", async (req, res) => {
+
+    await productRef.doc(req.params.id).delete()
+        .then(()=>res.status(204).send("Product successfully deleted!"))
+        .catch(error =>
+            res.status(500).send(error));
+});
+
+exports.api= functions.https.onRequest(app);
